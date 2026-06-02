9c435aceffb1b9aa05e2ff64da2937e451392b4e
aashish <ritikkamboj4314@gmail.com>
2026-06-02 16:13:31 +0530

    feat : password encryted and then saved in db by using beforeCreate function

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const {ServerConfig} = require("../config/")
+const bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +36,22 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+// sequelize.addHook("beforeCreate", async (User)=>{
+
+//   console.log("user value before entering in Db", User);
+
+//  const encryptedPassword = await bcrypt.hashSync(User.password , +ServerConfig.SALT_ROUND);
+//  User.password = encryptedPassword
+
+//   console.log("User object after password getting encrypted ", User)
+
+// })
+// above way is working, but its global hook and runs for every create operation in our app , ( ineffective approach )
+
+User.beforeCreate(async (user)=>{
+  console.log("user value before password getting encrypted",)
+  user.password = await bcrypt.hash(user.password, +ServerConfig.SALT_ROUND);
+  console.log("user value after password getting encrypted", user)
+})
   return User;
 };
