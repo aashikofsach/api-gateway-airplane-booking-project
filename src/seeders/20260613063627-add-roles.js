55be018084ba78dc8d80ccc94193734aa36c3268
aashish <ritikkamboj4314@gmail.com>
2026-06-13 12:26:16 +0530

    feat: add seed file for Roles with admin, customer, and flight_company entries

---
+++ src/seeders/20260613063627-add-roles.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    //  * Add seed commands here.
+    //  *
+    //  * Example:
+    await queryInterface.bulkInsert(
+      "Roles",
+      [
+        {
+          name: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "customer",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "flight_company",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    //  * Add commands to revert seed here.
+    //  *
+    //  * Example:
+    await queryInterface.bulkDelete("Roles", null, {});
+  },
+};
